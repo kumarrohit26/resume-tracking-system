107345968c074eeb3c4bf05c2cd9f625c6f1614d
Rohit Kumar <rohit.kr.0310@gmail.com>
2024-01-21 20:39:16 +0100

    streamlit app created

--- app.py
+++ app.py
@@ -6,5 +6,99 @@ import streamlit as st
 import os
 from PIL import Image
 import pdf2image
+import PyPDF2 as pdf
 import google.generativeai as genai
+import io
+import base64
 
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_response(input, pdf_content, prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input, pdf_content, prompt])
+    return response
+
+def input_pdf_setup(uploaded_file):
+
+    if uploaded_file:
+        # Convert pdf to image
+        pdf_image = pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page = pdf_image[0]
+
+        # convert image to bytes
+
+        image_byte_arr = io.BytesIO()
+        first_page.save(image_byte_arr, format='JPEG')
+        image_byte_arr = image_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(image_byte_arr).decode() # encode to base64
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError('No file uploaded')
+
+def input_pdf_text(uploaded_file):
+
+    if uploaded_file:
+        # Convert pdf to image
+        reader = pdf.PdfReader(uploaded_file)
+
+        text = ""
+
+        for page in range(len(reader.pages)):
+            page = reader.pages[page]
+            text += str(page.extract_text())
+        return text
+
+
+# Streamlit app
+
+st.set_page_config(page_title='ATS Resume Expert')
+st.header("ATS Scanner")
+input_text = st.text_area('Enter Job Description', key='input')
+uploaded_file = st.file_uploader('Upload Resume (PDF)', type=['pdf'])
+
+if uploaded_file is not None:
+    st.write('PDF Uploaded Successfully')
+
+submit1 = st.button('Tell me about the resume')
+submit2 = st.button('How Can I Improvise My Skills')
+#submit3 = st.button('What are the Keyords That are Missing')
+#submit4 = st.button('Percentage Match')
+
+input_prompt1 = """
+ You are an experienced Technical Human Resource Manager in the field of Data science, Data engineering, Data Analyst, 
+ Full stack development, software development, devops. your task is to review the provided resume against the job description. 
+Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+ Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+"""
+
+input_prompt3 = """
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of in the field of Data science, 
+Data engineering, Data Analyst, Full stack development, software development, devops and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_text(uploaded_file)
+        response=get_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
+
+elif submit4:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_text(uploaded_file)
+        response=get_response(input_prompt3,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
