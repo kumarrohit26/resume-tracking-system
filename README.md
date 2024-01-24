a0fdabd6504c04a9fa64d77a05fb751e6e7e3f60
Rohit Kumar <rohit.kr.0310@gmail.com>
2024-01-24 08:00:22 +0530

    setup changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # resume-tracking-system
+
+```
+conda create -p venv python==3.10 -y
+```
+
+```
+conda activate ./venv
+```
+
+```
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -87,7 +87,7 @@ the job description. First the output should come as percentage and then keyword
 
 if submit1:
     if uploaded_file is not None:
-        pdf_content=input_pdf_text(uploaded_file)
+        pdf_content=input_pdf_setup(uploaded_file)
         response=get_response(input_prompt1,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
@@ -96,7 +96,7 @@ if submit1:
 
 elif submit4:
     if uploaded_file is not None:
-        pdf_content=input_pdf_text(uploaded_file)
+        pdf_content=input_pdf_setup(uploaded_file)
         response=get_response(input_prompt3,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
